87c285717c9a510b0877050a47347a7e177cf6da
Sydney_Hunter <sydney.jane.hunter@gmail.com>
2020-04-23 14:59:51 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :appointments
+  resources :patients
+  resources :doctors
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :doctors
   resources :patients
